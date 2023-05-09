b8c27774af017ac2513cdd4ac6f454a4dc3c5507
Anwesha Pramanik <96810155+Anwesha05@users.noreply.github.com>
2023-05-09 21:48:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+model = pickle.load(open('cropPredict_ML_Model3.pkl','rb'))
+
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return "Hello anu"
+@app.route('/predict',methods=['POST'])
+def predict():
+    N = request.form.get('N')
+    P = request.form.get('P')
+    K = request.form.get('K')
+    # print(N)
+    # print(P)
+    # print(K)
+    temperature = request.form.get('temp')
+    print(temperature)
+    humidity = request.form.get('humidity')
+    ph = request.form.get('ph')
+    rainfall = request.form.get('rainfall')
+
+    input_query = np.array([[int(N),int(P),int(K),float(temperature),float(humidity),float(ph),float(rainfall)]])
+    result = model.predict(input_query)[0]
+    print(jsonify(result))
+    return jsonify({'crop_name':result})
+if __name__ == '__main__':
+    app.run(debug=True)
